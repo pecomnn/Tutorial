9a67b9a0dbfbb405a4c7ba48ae8780c8f9668e43
dmitriy <pecomnn@gmail.com>
2017-05-24 19:07:41 +0300

    First commit

--- lesson6.py
+++ lesson6.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 
+# "while" sentence
+
 def hello(name):
     print("Hello, " + name + " !")
 
 def main():
     print("What is your name?")
     name = input()
-    hello(name)
+    if name == "Dima":
+        hello(name)
+    elif name == "Vova":
+        print("Hey, Vova")
+    else:
+        print("By-by")
 
 if __name__ == "__main__":
     main()
